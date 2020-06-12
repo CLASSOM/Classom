0bac0f8ecf298da8f3798f5482560f2aee4985e8
hihiroo <alsrud606@naver.com>
2020-06-12 21:11:03 +0900

    main.js / post request 처리 추가

--- Backend/main.js
+++ Backend/main.js
@@ -1,19 +1,32 @@
 var http = require('http');
 var fs = require('fs');
+var qs = require('querystring');
 
 var app = http.createServer(function (request, response) {
     var url = request.url;
-    if (request.url == '/') {
+    if (request.url === '/') {
         url = '/test.html'; // 메인 html 페이지
+    
+        response.writeHead(200);
+        response.end(fs.readFileSync(__dirname + url));
+    }
+    else if(request.url === '/submit'){
+        var body = '';
+
+        request.on('data',function(data){
+            body = body + data;
+        });
+        
+        request.on('end',function(){
+            var post = qs.parse(body);
+            response.writeHead(200);
+            response.end(post.month_consumption);
+        });
+        
     }
     else{
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
-
 });
 app.listen(3000);

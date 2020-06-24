e2f8d4e8cf843a230541e2028a9b4f411d3b1853
hihiroo <alsrud606@naver.com>
2020-06-25 05:04:23 +0900

    db.js / 데베 파일 모듈화"

--- Backend/db.js
+++ Backend/db.js
@@ -1,24 +1,29 @@
 var sql = require('mysql');
 
-const db = sql.createConnection({
+/* db 서버 연결 */
+const config = sql.createConnection({
     host: "localhost",
     user: "root", 
     password: "1234qwer", // you should change this.
     database: "cardDB"
 });
 
-db.connect();
+config.connect();
 
-var dictBenefit = {};
+function find_best(res){
+    /* DB에서 할인율 가져와서 베스트 카드 계산 */
+    var dictBenefit = {};
 
-db.query('SELECT * from annual', function (err, rows, fields){
-    if (err) {
-        throw err;
-    }
+    // 연회비
+    config.query('SELECT * from annual', function (err, rows, fields){
+        if (err) {throw err;}
 
-    for (var i in rows){
-        dictBenefit[rows[i].카드명] = -rows[i].연회비;
-        console.log(rows[i].카드명 + ' ' + dictBenefit[rows[i].카드명]);
-    }
-});
+        for (var i in rows){
+            dictBenefit[rows[i].카드명] = -rows[i].annual_fee;
+            console.log(dictBenefit[rows[i].카드명]);
+        }
+    });
+}
+module.exports = find_best;
+
 

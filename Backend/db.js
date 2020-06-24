21cc1d35d6a8df9ccc2070d25cba9577f26d6f70
hihiroo <alsrud606@naver.com>
2020-06-24 18:00:02 +0900

    db.js / 카드 별 이율 저장하는 딕셔너리 생성 및 annual 테이블 쿼리 처리

--- Backend/db.js
+++ Backend/db.js
@@ -2,16 +2,23 @@ var sql = require('mysql');
 
 const db = sql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "1234qwer",
-    database: "CardDB"
+    user: "root", 
+    password: "1234qwer", // you should change this.
+    database: "cardDB"
 });
 
 db.connect();
 
-db.query('SELECT * from cardname', function (err, rows, fields){
+var dictBenefit = {};
+
+db.query('SELECT * from annual', function (err, rows, fields){
     if (err) {
         throw err;
     }
-    console.log(rows);
+
+    for (var i in rows){
+        dictBenefit[rows[i].카드명] = -rows[i].연회비;
+        console.log(rows[i].카드명 + ' ' + dictBenefit[rows[i].카드명]);
+    }
 });
+

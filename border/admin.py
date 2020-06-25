b190d36f104fd2d867329bb249e77f5e3317736b
hihiroo <alsrud606@naver.com>
2020-06-25 21:33:58 +0900

    필요없는 폴더 정리

--- border/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from border.models import Border
-# Register your models here.
-
-class  BorderAdmin(admin.ModelAdmin):
-    list_display=('id','제목', '작성자', '조회수')
-
-admin.site.register(Border, BorderAdmin)

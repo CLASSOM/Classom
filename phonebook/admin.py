9a3d31696da79ee21dae2f5bf4c0297de4932e7c
hihiroo <alsrud606@naver.com>
2020-06-25 21:31:40 +0900

    db, frontend 폴더 데이터도 정리

--- phonebook/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from phonebook.models import PhoneBook
-# Register your models here.
-
-class PhoneBookAdmin(admin.ModelAdmin):
-    list_display=('id','이름','전화번호','이메일','생년월일')
-
-admin.site.register(PhoneBook,PhoneBookAdmin)
